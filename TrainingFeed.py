203693c6bd76132ff223ad79c1d2743cf906a8a4
codewala-feed <codewala.info@gmail.com>
2025-04-25 12:04:52 +0530

    day 25

--- TrainingFeed.py
+++ TrainingFeed.py
@@ -1,6 +1,5 @@
-def data(name, age, gender):
-    return f"Hello {name}, You are {age} years old and you are {gender}"
-
-print(data("Akhil", 26, "Male"))
-print(data("Nikhil", 24, "Male"))
-print(data("Priya", 22, "Female"))
+def nums(n):
+    if (n > 0):
+        print(n)
+        nums(n-1) 
+nums(5)
